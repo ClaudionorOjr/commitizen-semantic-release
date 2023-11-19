25f5ce842c0254eec2f04ad7d39fd68ec626cf48
Claudionor Oliveira <claudionorojr@hotmail.com>
2023-11-19 17:51:13 -0300

    test(e2e): change e2e test

--- src/e2e-tests/e2e-test.spec.ts
+++ src/e2e-tests/e2e-test.spec.ts
@@ -4,4 +4,8 @@ describe('Contém um conjunto de testes, que será executado um a um', () => {
   it('Deve somar 20 + 50 e retornar 70', () => {
     expect(20 + 50).toBe(70)
   })
+
+  it("Deve concatenar a palavra 'Olá' com a palavra 'Mundo' e retornar 'Olá Mundo'", () => {
+    expect('Olá ' + 'Mundo').toBe('Olá Mundo')
+  })
 })
