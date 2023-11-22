71b5e8f650fd8de969ec681e740977122dc5be62
Claudionor Oliveira <claudionorojr@hotmail.com>
2023-11-21 22:08:31 -0300

    feat(test): fixed e2e test

--- src/e2e-tests/e2e-test.spec.ts
+++ src/e2e-tests/e2e-test.spec.ts
@@ -2,7 +2,7 @@ import { describe, expect, it } from 'vitest'
 
 describe('Contém um conjunto de testes, que será executado um a um', () => {
   it('Deve somar 20 + 50 e retornar 70', () => {
-    expect(20 + 50).toBe(0)
+    expect(20 + 50).toBe(70)
   })
 
   it("Deve concatenar a palavra 'Olá' com a palavra 'Mundo' e retornar 'Olá Mundo'", () => {
